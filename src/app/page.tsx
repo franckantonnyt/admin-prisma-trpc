124c2f20563246e06bba0532b42ac5e4189abb7d
franckantonnyt <franckantonnyt@outlook.com>
2024-08-14 20:56:17 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+'use cliente'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p>teste</p>
+    </main>
+  );
+}
